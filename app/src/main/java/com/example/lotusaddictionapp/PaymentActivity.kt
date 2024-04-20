980d6ff51a0a8dfadac5e33c8911ae8d4ca66f37
xavier <xaliivam@gmail.com>
2024-04-20 10:55:26 -0600

    Should be pushed now

--- app/src/main/java/com/example/lotusaddictionapp/PaymentActivity.kt
+++ app/src/main/java/com/example/lotusaddictionapp/PaymentActivity.kt
@@ -20,7 +20,7 @@ class PaymentActivity : AppCompatActivity() {
             all of the logic to handle filling in all of the boxes and saving them
             to a json file/database should be done here probably.
             Could maybe be done seperately from the button, but I believe the button
-            is the best trigger for this
+            is the best trigger for this.
              */
 
             val intent = Intent(this, Payment2Activity::class.java)
