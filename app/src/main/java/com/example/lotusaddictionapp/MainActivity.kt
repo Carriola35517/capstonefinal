fb5571ff6e30b26c98441f5ed639a6b43face4ec
xavier <xaliivam@gmail.com>
2024-04-24 16:24:07 -0600

    Should be runnable now

--- app/src/main/java/com/example/lotusaddictionapp/MainActivity.kt
+++ app/src/main/java/com/example/lotusaddictionapp/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.lotusaddictionapp
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -12,7 +14,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.lotusaddictionapp.ui.theme.LotusAddictionAppTheme
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -23,6 +25,10 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
+        val intent = Intent(this, WelcomeActivity::class.java)
+        startActivity(intent)
+        //finish MainActivity to prevent it from staying in the back stack
+        finish()
     }
 }
 
--- app/src/main/java/com/example/lotusaddictionapp/WelcomeActivity.kt
+++ app/src/main/java/com/example/lotusaddictionapp/WelcomeActivity.kt
@@ -1,11 +1,20 @@
 package com.example.lotusaddictionapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class WelcomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.welcome)
+
+        val registerBtn = findViewById<Button>(R.id.registerButton)
+        registerBtn.setOnClickListener{
+            println("Register clicked in LoginActivity")
+            val intent = Intent(this, RegisterActivity::class.java)//I've never used kotlin much, but can I still just use class.java here without any problems?
+            startActivity(intent) //start up the registration activity
+        }
     }
 }
